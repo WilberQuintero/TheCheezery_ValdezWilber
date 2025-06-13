fb0428a64baf20f08850b1f90bbff80c53759390
WilberQuintero <wilber.valdez247180@potros.itson.edu.mx>
2025-06-13 13:04:28 -0700

    listviews
    Práctica 5 - The Cheezery has products

--- TheCheezery/app/src/main/java/valdez/wilber/thecheezery/MenuActivity.kt
+++ TheCheezery/app/src/main/java/valdez/wilber/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package valdez.wilber.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,16 @@ import androidx.core.view.WindowInsetsCompat
             override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_menu)
+
+
+                var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+
+                btnCold.setOnClickListener{
+                    var intent: Intent = Intent(this, ProductosActivity:: class.java)
+                    startActivity(intent)
+                }
+
+
             }
         }
 
---
+++ TheCheezery/app/src/main/java/valdez/wilber/thecheezery/Product.kt
@@ -0,0 +1,11 @@
+package valdez.wilber.thecheezery
+
+import android.media.Image
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
+
+
+
